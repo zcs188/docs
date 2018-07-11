137a3bbc2e4f14f8eded3b95cfde53002c7209c3
baijunyao <baijunyao@baijunyao.com>
2018-07-11 18:01:42 +0800

    :tada: 初始化完成

---
+++ laravel-print/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    base: '/docs/laravel-print/',
+    title: 'laravel-print',
+    description: '以简洁的方式打印数据',
+    head: [
+        ['link', { rel: 'icon', href: `/favicon.ico` }],
+    ],
+    dest: 'dist/laravel-print',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: '白俊遥博客', link: 'https://baijunyao.com' },
+        ],
+        sidebar: [
+            {
+                title: '安装使用',
+                collapsable: false,
+                children: [
+                    ['安装', '安装']
+                ]
+            },
+        ],
+    },
+}
