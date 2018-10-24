389ab3d0a79b1ebffabc205192fb985d68702c71
baijunyao <baijunyao@baijunyao.com>
2018-10-24 22:13:55 +0800

    增加thinkphp-bjyadmin文档

---
+++ thinkphp-bjyadmin/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    base: '/docs/thinkphp-bjyadmin/',
+    title: 'thinkphp-bjyadmin',
+    description: '以简洁的方式打印数据',
+    head: [
+        ['link', { rel: 'icon', href: `/favicon.ico` }],
+    ],
+    dest: 'dist/thinkphp-bjyadmin',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: '白俊遥博客', link: 'https://baijunyao.com' },
+        ],
+    },
+}
